9ec1a05be1bc84fa3ddf1b6e7edf5060ee073013
Golam Choudhury <golamch@outlook.com>
2024-03-22 15:44:25 +0000

    separating out method logic

--- src/main/java/com/golamch/Raindrops.java
+++ src/main/java/com/golamch/Raindrops.java
@@ -4,23 +4,10 @@ public class Raindrops {
 
     public String plingPlangPlong(int n) {
 
-        int[] remainders = findRemainders(n);
-
-        String[] dropletSounds = {"Pling", "Plang", "Plong"};
-
-        StringBuilder stringBuilder = new StringBuilder();
-
-        for (int i = 0; i <= remainders.length - 1; i++) {
-            if (remainders[i] == 0) {
-                stringBuilder.append(dropletSounds[i]);
-            }
-        }
-
-        // Handling special case of zero
-        if (n == 0 || stringBuilder.toString().equals("")) {
+        if (n == 0 || buildRaindropSounds(findRemainders(n)).equals("")) {
             return Integer.toString(n);
         } else {
-            return stringBuilder.toString();
+            return buildRaindropSounds(findRemainders(n));
         }
     }
 
@@ -33,6 +20,27 @@ public class Raindrops {
         int modThree = n % 3, modFive = n % 5, modSeven = n % 7;
 
         return new int[]{modThree, modFive, modSeven};
+    }
+
+    /**
+     * Returns a string depending on the list of remainders passed to the method. The resulting string will either be
+     * empty, Pling, Plang, Plong, or a combination (preserving the listed order).
+     * @param remaindersList this integer array is the result from checking the remainders when dividing an inetger by
+     *                       3, 5 & 7.
+     * @return a string which is either empty or a combination of Pling, Plang or Plong
+     */
+    private String buildRaindropSounds(int[] remaindersList) {
+        String[] dropletSounds = {"Pling", "Plang", "Plong"};
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i <= remaindersList.length - 1; i++) {
+            if (remaindersList[i] == 0) {
+                stringBuilder.append(dropletSounds[i]);
+            }
+        }
+
+        return stringBuilder.toString();
     }
 
 }
